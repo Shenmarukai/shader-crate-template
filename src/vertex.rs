8aa6f45e0e3a2be71a5bf0aaf1c6709edb6c5fe7
Shenmarukai <shanemulc@comcast.net>
2025-02-04 17:42:54 -0500

    init

---
+++ src/vertex.rs
@@ -0,0 +1,30 @@
+use glam::{Vec2, Vec4};
+use spirv_std::spirv;
+
+pub const CLIP_SPACE_COORD_QUAD_CCW: [ Vec4; 6 ] = {
+    let tl = Vec4::new( -1.0, 1.0, 0.5, 1.0 );
+    let tr = Vec4::new( 1.0, 1.0, 0.5, 1.0 );
+    let bl = Vec4::new( -1.0, -1.0, 0.5, 1.0 );
+    let br = Vec4::new( 1.0, -1.0, 0.5, 1.0 );
+    [ bl, br, tr, tr, tl, bl ]
+};
+
+pub const UV_COORD_QUAD_CCW: [ Vec2; 6 ] = {
+    let tl = Vec2::new( 0.0, 0.0 );
+    let tr = Vec2::new( 1.0, 0.0 );
+    let bl = Vec2::new( 0.0, 1.0 );
+    let br = Vec2::new( 1.0, 1.0 );
+    [ bl, br, tr, tr, tl, bl ]
+};
+
+/// Vertex shader that renders an implicit quad.
+#[spirv( vertex )]
+pub fn vertex(
+    #[spirv( vertex_index )] vertex_id: u32,
+    out_uv: &mut Vec2,
+    #[spirv( position )] clip_pos: &mut Vec4,
+) {
+    let index = vertex_id as usize % 6;
+    *out_uv = UV_COORD_QUAD_CCW[ index ];
+    *clip_pos = CLIP_SPACE_COORD_QUAD_CCW[ index ];
+}
